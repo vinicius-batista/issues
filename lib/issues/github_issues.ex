1b4c0e2cda79cd98386766e156103280347cb8c0
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-08-04 11:19:20 -0300

    github_issues: add module to fetch issues from github

---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,26 @@
+defmodule Issues.GithubIssues do
+  alias Poison.Parser
+
+  @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
+  @github_url Application.get_env(:issues, :github_url)
+
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response()
+  end
+
+  def issues_url(user, project) do
+    "#{@github_url}/repos/#{user}/#{project}/issues"
+  end
+
+  def handle_response({_, %{status_code: status_code, body: body}}) do
+    {
+      status_code |> check_for_error,
+      body |> Parser.parse!()
+    }
+  end
+
+  def check_for_error(200), do: :ok
+  def check_for_error(_), do: :error
+end
