c7fbf141ebaea9bad5981d3e3621d478145ec760
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-08-05 16:39:08 -0300

    chore: add logger

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,10 +1,14 @@
 defmodule Issues.GithubIssues do
   alias Poison.Parser
 
+  require Logger
+
   @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info("Fetching #{user}'s project #{project}")
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response()
@@ -15,6 +19,9 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({_, %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status code=#{status_code}")
+    Logger.debug(fn -> inspect(body) end)
+
     {
       status_code |> check_for_error,
       body |> Parser.parse!()
