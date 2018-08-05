bc10986c788fb05d72850a4cb1e89a33c527311d
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-08-05 15:38:02 -0300

    cli: add last method

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -40,10 +40,11 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
     |> sort_into_descending_order()
+    |> last(count)
   end
 
   def decode_response({:ok, body}), do: body
@@ -56,5 +57,11 @@ defmodule Issues.CLI do
   def sort_into_descending_order(list_of_issues) do
     list_of_issues
     |> Enum.sort(&(&1["created_at"] >= &2["created_at"]))
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse()
   end
 end
